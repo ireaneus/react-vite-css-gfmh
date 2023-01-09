0d0d4f08c71314d418bcd81ba5a483c5825cb83d
ireaneus <bwdave@protonmail.com>
2023-01-09 14:51:42 -0700

    5 files modified, 3 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
-  return <>Hello World</>
+  const subtitle = 'A church body worshipping God in Spirit and Truth';
+  
+  return (
+    <>
+      <Header subtitle={subtitle} />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './styles/styles.scss'
+import 'normalize.css';
+import './styles/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
